3352aae706ec45badf190c89f5448f9539ad2937
Sasha Chepurniy <sloth04ua@gmail.com>
2021-12-08 14:14:53 +0300

    Added logger, added DB

--- main.py
+++ main.py
@@ -3,46 +3,48 @@ import pandas as pd
 import sys
 import logging
 from pathlib import Path
-from logging import StreamHandler
+from logging import StreamHandler, Formatter
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_NAME = 'Task_06_12.sqlite'
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = StreamHandler(stream=sys.stdout)
+handler.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 logger.addHandler(handler)
 
-symmetric_dict = {'FCR': 'РПЧ', 'aFRR': 'аРВЧ', 'mFRR': 'рРВЧ'}
+type_dict = {'FCR': 'РПЧ', 'aFRR': 'аРВЧ', 'mFRR': 'рРВЧ'}
+result_columns = ['Market Participant', 'Service Type', 'Resource', 'Date', 'Settlement Period', 'Direction']
 
 
-# def manipulation(df, time_item, name, direction):
-#     res = pd.DataFrame()
-#     for st_type in symmetric_dict.keys():
-#         temp = df[(df['Market Participant'] == name)
-#                   & (df['Service Type'] == st_type)
-#                   & (df.index == time_item)
-#                   & (df['Direction'] == direction)]
-#         result = temp.groupby(pd.Grouper(freq='H')).sum()
-#         res.append(result)
-#     return res
+def create_index(df):
+    df['Datetime'] = df.apply(lambda x: str(x['Date'])[:10] + ' ' + x['Settlement Period'], axis=1)
+    df.index = df['Datetime']
+    df.index = pd.to_datetime(df.index.str[:-6], format='%Y-%m-%d %H:%M')
+    logger.debug('Index column was created using Date and Settlement Period')
+    df = df.drop(columns=['Datetime'])
+    return df
 
 
 def select(df):
-    df_new = pd.DataFrame()
-    # for item in df_new.index.unique():
-    #     for name in np.sort(df['Market Participant'].unique()):
-    #         for direction in df['Direction'].unique():
-    #             df_new.append(manipulation(df, item, name, direction))
-    df_new = df.groupby(['Market Participant', 'Service Type', 'Date', 'Settlement Period', 'Direction'], as_index=False)[['Quantity (MWh)', 'Ancillary Service Payment (₴)']].sum()
-    print(df_new)
+    df_new = df.groupby(result_columns, as_index=False)[['Quantity (MWh)', 'Ancillary Service Payment (₴)']].sum()
+    create_index(df_new)
+    logger.debug(f'New dataframe was created with columns {result_columns}')
+    # df_new['Date'] = pd.to_datetime(str(df['Date']), format='%Y-%m-%d') # cant reformat into datetime
+    df_new = df_new.drop(columns=['Datetime'])  # while am deleting column in create_index() it is still appear in
+    # result*.xlsx
+    df_new['Service Type'] = df_new['Service Type'].replace(type_dict)
+    logger.debug(f'Service Type column was replaced using dictionary {type_dict}')
     return df_new
 
 
 def create_df(path):
     df = pd.read_excel(path, sheet_name='Sheet1')
-    df['Datetime'] = df.apply(lambda x: str(x['Date'])[:10] + ' ' + x['Settlement Period'], axis=1)
-    df.index = df['Datetime']
-    df.index = pd.to_datetime(df.index.str[:-6], format='%Y-%m-%d %H:%M')
-    df = df.drop(columns=['Datetime'])
-    print(df)
+    logger.debug(f'Dataframe was created using file by {path}')
+    create_index(df)
     return df
 
 
@@ -50,12 +52,21 @@ def main():
     num = 0
     dir_list = Path.cwd().rglob('input*.xlsx')
     output = Path.cwd() / 'output'
+    logger.info(f'Founded {len(list(dir_list))} file/-s')
     output.mkdir(parents=True, exist_ok=True)
+    logger.info('Output dir is created ')
+    engine = create_engine(f'sqlite:///{output}\\{DATABASE_NAME}', echo=False)
+    logger.info(f'Database {DATABASE_NAME} is created by path {output}')
     for item in dir_list:
         df = create_df(item)
         df = select(df)
         num += 1
-        df.to_excel(f'{output}\\result{num}.xlsx')
+        file_name = f'result{num}'
+        df.to_sql(file_name, index=False, con=engine, if_exists='replace')
+        engine.execute(f'SELECT * FROM {file_name}').fetchall()
+        logger.debug(f'SQL table named {file_name} is created')
+        df.to_excel(f'{output}\\{file_name}.xlsx', index=False)
+        logger.debug(f'Excel table named {file_name}.xlsx is created')
 
 
 if __name__ == '__main__':
