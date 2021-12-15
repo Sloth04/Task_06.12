54107d91cade51912de72b8b2f4e2d32d1654d37
Sasha Chepurniy <sloth04ua@gmail.com>
2021-12-15 19:44:04 +0300

    Fixed

--- config.py
+++ config.py
@@ -9,8 +9,13 @@ handler = StreamHandler(stream=sys.stdout)
 handler.setFormatter(Formatter(fmt='[%(asctime)s: %(levelname)s] %(message)s'))
 logger.addHandler(handler)
 
-DATABASE_NAME = 'Task_06_12.sqlite' # to config
-result_columns = ['company_alias', 'y_code', 'service_type', 'w_code', 'datetime', 'direction']  # to config
-df_n_columns = ['company_alias', 'y_code', 'service_type', 'w_code', 'date', 'time', 'direction', 'volume', 'payment']  # to config
-y_code_dict = {'10Y1001C--000182': 'CA_UA_IPS', '10YUA-WEPS-----0': 'CA_UA_BEI'}  # to config
-output = Path.cwd() / 'output'  # to config
+output = Path.cwd() / 'output'
+
+DATABASE_NAME = 'Task_06_12.sqlite'
+
+result_columns = ['company_alias', 'y_code', 'service_type', 'w_code', 'datetime', 'direction']
+df_n_columns = ['company_alias', 'y_code', 'service_type', 'w_code', 'date', 'time', 'direction', 'volume', 'payment']
+y_code_dict = {'10Y1001C--000182': 'CA_UA_IPS', '10YUA-WEPS-----0': 'CA_UA_BEI'}
+
+service_type_dict = {'FCR': 'РПЧ', 'aFRR': 'аРВЧ', 'mFRR': 'рРВЧ'}
+direction_dict = {'Symmetric': 'Симетрично', 'Up': 'Вгору', 'Down': 'Вниз'}
--- report.py
+++ report.py
@@ -1,21 +1,17 @@
 import pandas as pd
 from main import Records, Models, Resourse
 from main import session
-from main import output
-
-day_report = '2021-05-16'  # param to report
-service_type_dict = {'FCR': 'РПЧ', 'aFRR': 'аРВЧ', 'mFRR': 'рРВЧ'}  # to config
-direction_dict = {'Symmetric': 'Симетрично', 'Up': 'Вгору', 'Down': 'Вниз'}  # to config
+from config import *
 
 
-def report():
-    file_name = 'report_for_'+day_report
+def report(day):
+    file_name = 'report_for_'+day
     q = session.query(Resourse.company_alias, Resourse.y_code,
                       Models.service_type, Records.w_code, Records.datetime,
                       Models.direction, Records.volume, Records.payment)\
         .join(Models)\
         .join(Resourse)\
-        .filter(Records.datetime.like(f'{day_report}%'))
+        .filter(Records.datetime.like(f'{day}%'))
     df = pd.read_sql(q.statement, session.bind)
     df['service_type'] = df['service_type'].replace(service_type_dict)
     df['direction'] = df['direction'].replace(direction_dict)
@@ -23,4 +19,5 @@ def report():
 
 
 if __name__ == '__main__':
-    report()
+    day_report = '2021-05-16'
+    report(day_report)
